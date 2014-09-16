7a5f4be0859b451cba1c6814c0a7d54ca78937bb
Rolive0712 <roshitrajan@rediffmail.com>
2014-09-16 14:04:48 -0400

    Update MVC-AngularPost-ComplexModelBinding.cs

--- MVC-AngularPost-ComplexModelBinding.cs
+++ MVC-AngularPost-ComplexModelBinding.cs
@@ -30,3 +30,9 @@ At angular service =>
 
 Note:
 The List<T> in controller can have multiple child objects in it.
+Some serialization links:
+1. http://stackoverflow.com/questions/20038441/deserialize-a-listabstractclass-with-newtonsoft-json
+2. http://stackoverflow.com/questions/17126323/parsing-a-complex-json-result-with-c-sharp
+3. http://www.codeproject.com/Tips/79435/Deserialize-JSON-with-C
+4. http://philcurnow.wordpress.com/2013/12/29/serializing-and-deserializing-json-in-c/
+5. http://www.cshandler.com/2013/09/deserialize-list-of-json-objects-as.html
