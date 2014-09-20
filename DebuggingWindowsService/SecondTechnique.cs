cd4ec12933c1e8381fe2f2bb73d329e36c77aee2
Rolive0712 <roshitrajan@rediffmail.com>
2014-09-20 17:10:11 -0400

    Create SecondTechnique.cs

---
+++ DebuggingWindowsService/SecondTechnique.cs
@@ -0,0 +1,39 @@
+ static void Main()
+        {
+         
+#if (!DEBUG)
+    System.ServiceProcess.ServiceBase[] ServicesToRun;
+    ServicesToRun = new System.ServiceProcess.ServiceBase[] { new Service1() };
+    System.ServiceProcess.ServiceBase.Run(ServicesToRun);
+#else
+            // Debug code: this allows the process to run as a non-service.
+            // It will kick off the service start point, but never kill it.
+            // Shut down the debugger to exit
+            Service1 service = new Service1();
+            service.Start();
+            // Put a breakpoint on the following line to always catch
+            // your service when it has finished its work
+            System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
+#endif
+        }
+        
+/// <summary>
+/// Call this method directly while debugging from program class.
+/// </summary>      
+public void Start()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        protected override void OnStart(string[] args)
+        {
+            Start();
+        }
+
+        protected override void OnStop()
+        {
+        }
