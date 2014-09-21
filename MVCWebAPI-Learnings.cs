2640feea9e5d99992e350a08a360cf1c390cabd5
Rolive0712 <roshitrajan@rediffmail.com>
2014-09-20 21:05:09 -0400

    Update and rename MVCWebAPI-Learnings.html to MVCWebAPI-Learnings.cs

--- MVCWebAPI-Learnings.cs
+++ MVCWebAPI-Learnings.cs
@@ -1,12 +1,12 @@
-* Use MVC Web API if pages load large datasets/json's. Large means data rows exceeding 5000 records in table.
-* Use Regular MVC controller only for smaller applications which is not data centric.
-* There is restriction to the size of JSON data that can be returned from MVC controller JSONResult Action method witin regular
-  MVC controller.
-* Both MVC regular controller & Web API controller can be used within an app.
+/*Use MVC Web API if pages load large datasets/json's. Large means data rows exceeding 5000 records in table.
+ * Use Regular MVC controller only for smaller applications which is not data centric.
+ * There is restriction to the size of JSON data that can be returned from MVC controller JSONResult Action method witin regular
+    MVC controller.
+ * Both MVC regular controller & Web API controller can be used within an app.*/
 
-Code Conventions:
-Regular MVC Controller:
-*  public class ChartsController : Controller
+//Code Conventions:
+//Regular MVC Controller:
+public class ChartsController : Controller
     {
         public JsonResult GetMonthWiseRiskIssueCount(<params>)
         {
@@ -23,7 +23,7 @@ Regular MVC Controller:
   }
   
   
-Web API controller:
+//Web API controller:
 public class ChartsController : ApiController
     {
         [System.Web.Http.HttpGet]
