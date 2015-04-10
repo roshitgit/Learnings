db0bc430298b2c7308e946b9669d6df4fe1af261
Rolive0712 <roshitrajan@rediffmail.com>
2015-04-10 19:53:25 +0530

    Update GenerateXMLFromDataset.cs

--- Csharp/GenerateXMLFromDataset.cs
+++ Csharp/GenerateXMLFromDataset.cs
@@ -63,12 +63,12 @@ namespace GenerateXMLFromExcel
 
                     if (oledbConn.State == ConnectionState.Closed) oledbConn.Open();
 
-                    if (ExcelIsValid(oledbConn))
-                    {
-                        DataTable sheetTable = oledbConn.GetSchema("Tables");
-                        string strSHeetName = Convert.ToString(sheetTable.Rows[0]["TABLE_NAME"]);
+                    DataTable sheetTable = oledbConn.GetSchema("Tables");
+                    string strSHeetName = Convert.ToString(sheetTable.Rows[0]["TABLE_NAME"]);
 
-                        string selectQuery = "Select [COL1],[COL2],[COL3] from [" + strSHeetName + "]";
+                    if (ExcelIsValid(oledbConn, strSHeetName))
+                    {
+                        string selectQuery = "Select [COL1],[COL2] from [" + strSHeetName + "]";
 
                         OleDbDataAdapter da = new OleDbDataAdapter(selectQuery, oledbConn);
                         da.SelectCommand = oledbCmd;
@@ -106,9 +106,9 @@ namespace GenerateXMLFromExcel
                             }
                         }
 
-                        DataSet ds = new DataSet("MyDataSet");
+                        DataSet ds = new DataSet("AdhocData");
                         ds.Tables.Add(dtCloned);
-                        ds.Tables[0].TableName = "MyTable";
+                        ds.Tables[0].TableName = "Table";
 
                         string xml = ds.GetXml().Replace("_x0020_", "_");
                     }
@@ -127,19 +127,20 @@ namespace GenerateXMLFromExcel
         /// </summary>
         /// <param name="oledbConn"></param>
         /// <returns></returns>
-        private static bool ExcelIsValid(OleDbConnection oledbConn)
+        private static bool ExcelIsValid(OleDbConnection oledbConn, string sheetName)
         {
-            string[] ExcelColNames = { "Col1", "Col2", "Col3" };
+            string[] ExcelColNames = { "COL1", "COl2"};
 
             int validColCount = ExcelColNames.Length;
             bool FileIsValid = true;
-
-            //DataTable dtSchema = oledbConn.GetOleDbSchemaTable(OleDbSchemaGuid.Columns, null);
-
             List<string> fileColumns = new List<string>();
-            DataTable dtCols = oledbConn.GetSchema("Columns");
 
-            //string[] fileColumns = dtCols.Columns.AsEnumerable().Select(column => column.ColumnName).ToArray();
+
+            string[] restrictions = new string[4] { null, null, sheetName, null };
+            //Apply restrictions to get data only from the first sheet of excel file.
+            //these restrictions work even if there are blank excel sheets in files.
+            //if restrictions are not applied, then it gets junk columns from all available sheets in excel
+            DataTable dtCols = oledbConn.GetSchema("Columns", restrictions );
 
             foreach (DataRow dr in dtCols.Rows)
             {
