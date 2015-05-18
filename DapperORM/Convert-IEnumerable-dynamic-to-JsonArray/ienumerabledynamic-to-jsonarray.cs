616fb1b3673e94f8c54236a24ae789df279b008e
Rolive0712 <roshitrajan@rediffmail.com>
2015-05-18 23:07:49 +0530

    Create ienumerabledynamic-to-jsonarray.cs

---
+++ DapperORM/Convert-IEnumerable-dynamic-to-JsonArray/ienumerabledynamic-to-jsonarray.cs
@@ -0,0 +1,30 @@
+http://stackoverflow.com/questions/10046496/convert-ienumerabledynamic-to-jsonarray
+
+
+
+
+[WebGet(UriTemplate = "/tools/data/get?tool={tool}&filters={filters}")]
+    public JsonArray GetData(string tool, string[,] filters)
+    {
+        IEnumerable<dynamic> list = _repository.All("", "", 0).ToList();
+
+
+        IEnumerable<JsonObject> jsonList = from item in list
+                                select new JsonObject()
+                                           {
+                                               new KeyValuePair<string, JsonValue>("Id", item.Id),
+                                               new KeyValuePair<string, JsonValue>("Name", item.Title),
+                                               new KeyValuePair<string, JsonValue>("Data", new JsonObject()
+                                                                                               {
+                                                                                                    new KeyValuePair<string, JsonValue>("Product", item.Product),
+                                                                                                    new KeyValuePair<string, JsonValue>("Suite", item.Suite),
+                                                                                                    new KeyValuePair<string, JsonValue>("Package", item.Package),
+                                                                                                    new KeyValuePair<string, JsonValue>("Description", item.Description)
+                                                                                               })
+
+                                           };
+
+        JsonArray returnValue = new JsonArray(jsonList);
+
+        return returnValue;
+    }
