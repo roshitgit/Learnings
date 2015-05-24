0e1025aa7e8d55d041aeec8e9132a9acb56bb36b
Rolive0712 <roshitrajan@rediffmail.com>
2015-05-24 19:46:29 +0530

    Update ienumerabledynamic-to-jsonarray.cs

--- DapperORM/Convert-IEnumerable-dynamic-to-JsonArray/ienumerabledynamic-to-jsonarray.cs
+++ DapperORM/Convert-IEnumerable-dynamic-to-JsonArray/ienumerabledynamic-to-jsonarray.cs
@@ -25,7 +25,17 @@ http://stackoverflow.com/questions/10046496/convert-ienumerabledynamic-to-jsonar
 
         return returnValue;
     }
-    
+
+
+List<int> list = new List<int>() {1 , 2};
+string json = JsonConvert.SerializeObject(
+                  list.Select(x => new{
+                      id = x.ToString(),
+                      title = "title " + x.ToString(),
+                      data = Enumerable.Range(3,2).Select(i=> new {column1=i,column2=i*i})
+                    })
+                  , Newtonsoft.Json.Formatting.Indented
+                );   
     
     
 *** must implement - Getting dynamic ExpandoObject to serialize to JSON as expected
