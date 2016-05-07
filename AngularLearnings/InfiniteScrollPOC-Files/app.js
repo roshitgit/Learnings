f4e99a18d9ac69caba15fa2feebf59830dfdc17e
Rolive0712 <roshitrajan@rediffmail.com>
2016-05-07 19:57:40 +0530

    Create app.js

---
+++ AngularLearnings/InfiniteScrollPOC-Files/app.js
@@ -0,0 +1,26 @@
+var app = angular.module('app', ['ui.grid']);
+
+app.controller('MainCtrl', ['$scope', '$http', 'uiGridConstants', function ($scope, $http, uiGridConstants) {
+    $scope.gridOptions = {
+        enableFiltering: true,
+        flatEntityAccess: true,
+        showGridFooter: true,
+        fastWatch: true
+    };
+
+    $scope.gridOptions.columnDefs = [
+     { name: 'id' },
+     { name: 'name' },
+     { name: 'gender' },
+     { field: 'age' }
+    ];
+
+    $http.get('/api/Data/GetRecordsForUiGrid')
+    .success(function (data) {
+        $scope.gridOptions.data = data;
+    });
+
+    $scope.toggleFlat = function () {
+        $scope.gridOptions.flatEntityAccess = !$scope.gridOptions.flatEntityAccess;
+    }
+}]);
